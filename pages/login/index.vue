32f3e7f911a476d2ec1db928e5c20f4d185a7997
290517897@qq.com <290517897@qq.com>
2019-07-18 00:04:09 +0800

    11

---
+++ pages/login/index.vue
@@ -0,0 +1,60 @@
+<template>
+	<view style="padding-left: 10rpx;padding-right: 10rpx;background-color: #FFFFFF;">
+
+
+	</view>
+</template>
+
+<script>
+	import '@/static/css/style.css'
+
+
+	export default {
+		components: {
+
+		},
+		data() {
+			return {
+				current: 0,
+				info: [{
+					url: '../../static/images/footer1.png'
+				}, {
+					url: '../../static/images/footer1.png'
+				}, {
+					url: '../../static/images/footer1.png'
+				}],
+				showMembershipCode: false,
+				showMyInfo: false
+			}
+		},
+		onLoad() {
+			let this_ = this
+			wx.login({
+				success(res) {
+					console.log(res)
+					if (res.code) {
+						this_.$uniRequest.get('/api/small_procedures/login/login', {
+							data: {
+								code: res.code
+							}
+						}).then(res => {
+							wx.setStorage({key:'token',data:res.data})
+							console.log(response)
+						}).catch(error => {
+							console.log(error)
+						})
+					}
+				}
+			})
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style scoped>
+	image {
+		will-change: transform
+	}
+</style>
