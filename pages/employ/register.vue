dc89705524e383510e73b18347ead6accaa65161
290517897@qq.com <290517897@qq.com>
2019-07-18 23:15:29 +0800

    1

--- pages/employ/register.vue
+++ pages/employ/register.vue
@@ -2,22 +2,24 @@
 	<view style="">
 		<div style="padding-top: 15rpx;padding-left: 30rpx;padding-right: 30rpx;">
 			<van-cell-group>
-				<i-input v-model="employCode" title="收货人" autofocus placeholder="名字" />
-				<!-- <van-field :value="employCode" required clearable label="员工编号" placeholder="请输入员工编号" /> -->
+				<van-field @input="inputEmployCode" :value="employCode" required clearable label="员工编号" placeholder="请输入员工编号" />
 			</van-cell-group>
 		</div>
 		<div style="padding-top: 30rpx;padding-left: 30rpx;padding-right: 30rpx;">
 			<van-cell-group>
-				<van-button :disabled="employCode == ''" type="primary" size="large">绑定</van-button>
+				<van-button :loading="loading" loading-text="绑定中..." :disabled="employCode == ''" type="primary" size="large"
+				 open-type="getUserInfo" @click="doBind">绑定</van-button>
+				<van-button :loading="loading" loading-text="绑定中..." :disabled="employCode == ''" type="primary" size="large"
+				 open-type="getPhoneNumber" @getphonenumber="getPhoneNumber">绑定</van-button>
+
 			</van-cell-group>
 		</div>
-
 	</view>
 </template>
 
 <script>
 	import '@/static/css/style.css'
-
+	import Toast from '@/wxcomponents/vant/toast/toast';
 
 	export default {
 		components: {
@@ -25,8 +27,8 @@
 		},
 		data() {
 			return {
-				employCode:'',
-				current: 0,
+				employCode: '',
+				loading: false,
 				showMembershipCode: false,
 				showMyInfo: false
 			}
@@ -35,17 +37,53 @@
 
 		},
 		methods: {
-			onClickLeft() {
-				wx.showToast({
-					title: '点击返回',
-					icon: 'none'
+			getPhoneNumber(e) {
+				console.log(e)
+				console.log(e.detail.errMsg)
+				console.log(e.detail.iv)
+				console.log(e.detail.encryptedData)
+			},
+			inputEmployCode(e) {
+				this.employCode = e.detail
+			},
+			doBind() {
+				this.updateUserInfo()
+				// 查看是否授权
+				wx.getSetting({
+					success(res) {
+						if (res.authSetting['scope.userInfo']) {
+							wx.getUserInfo({
+								success: function(res) {
+									console.log(res.userInfo)
+								}
+							})
+						} else {
+							wx.authorize({
+								scope: 'scope.userInfo',
+								success() {
+									wx.getUserInfo({
+										success: function(res) {
+											console.log(res.userInfo)
+										}
+									})
+								},
+								fail() {
+									Toast('授权后可以绑定');
+								}
+							})
+						}
+					}
 				})
 			},
-			onClickRight() {
-				wx.showToast({
-					title: '点击按钮',
-					icon: 'none'
-				})
+			updateUserInfo() {
+				this.loading = true
+				this.$uniRequest.post('/api/small_procedures/employ/bind', {
+					data: {
+						code: this.employCode
+					}
+				}).then(res => {
+
+				}).finally(() => this.loading = false)
 			}
 		}
 	}
--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,6 +1,6 @@
 <template>
 	<view style="padding-left: 10rpx;padding-right: 10rpx;background-color: #FFFFFF;">
-		
+
 	</view>
 </template>
 
@@ -14,43 +14,72 @@
 		},
 		data() {
 			return {
-				current: 0,
+				scene: '',
 				showMembershipCode: false,
 				showMyInfo: false
 			}
 		},
 		onLoad(query) {
 			let this_ = this
-			let scene = ''
-			if(query.scene){
-				scene = decodeURIComponent(query.scene)
+			if (query.scene) {
+				this.scene = decodeURIComponent(query.scene)
 			}
-			console.log(scene)
-			wx.login({
-				success(res) {
-					if (res.code) {
-						this_.$uniRequest.get('/api/small_procedures/login/login', {
+			wx.checkSession({
+				success() {
+					let token = wx.getStorageSync('token')
+					if (token == null || token.openid == null) {
+						this_.login()
+					} else {
+						this_.$uniRequest.get('/api/small_procedures/login/login_by_openid', {
 							data: {
-								code: res.code
+								openid: token.openid
 							}
 						}).then(res => {
-							wx.setStorage({key:'token',data:res.data})
-							//登录成功后，判断参数内容
-							//是否为员工注册
-							if(scene.trim() == 'employ|register'){
-								//跳转到绑定页面
-								uni.redirectTo({url:'../employ/register'})
-							}	
-							console.log(res)
+							this_.loginOk(res.data)
 						}).catch(error => {
-							console.log(error)
+
 						})
 					}
+				},
+				fail() {
+					this_.login()
 				}
 			})
 		},
 		methods: {
-
+			login() {
+				let this_ = this
+				wx.login({
+					success(res) {
+						if (res.code) {
+							this_.$uniRequest.get('/api/small_procedures/login/login_by_code', {
+								data: {
+									code: res.code
+								}
+							}).then(res => {
+								this_.loginOk(res.data)
+								console.log(res)
+							}).catch(error => {
+								console.log(error)
+							})
+						}
+					}
+				})
+			},
+			loginOk(data) {
+				wx.setStorage({
+					key: 'token',
+					data: data
+				})
+				//登录成功后，判断参数内容
+				//是否为员工注册
+				if (this.scene.trim() == 'employ|register') {
+					//跳转到绑定页面
+					uni.redirectTo({
+						url: '../employ/register'
+					})
+				}
+			}
 		}
 	}
 </script>
